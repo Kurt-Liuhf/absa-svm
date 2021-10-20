68238b011a6948e8747ecb08f49e4d52810ee102
LarryHH <larryhieuhuynh@gmail.com>
2021-10-20 17:55:00 +0800

    edit path

--- dataset_stanza.py
+++ dataset_stanza.py
@@ -85,8 +85,8 @@ class Dataset(object):
             self.save_as_pickle(base_dir, 'parsed_data', '_parsed_train.plk', '_parsed_test.plk', self.train_data, self.test_data)
             self.save_as_txt(base_dir, 'parsed_data', '_parsed_train.txt', '_parsed_test.txt', self.train_data, self.test_data)
         else:
-            training_path = os.path.join('../', base_dir, 'parsed_data', '_parsed_train.plk')
-            test_path = os.path.join('../', base_dir, 'parsed_data', '_parsed_test.plk')
+            training_path = os.path.join(base_dir, 'parsed_data', '_parsed_train.plk')
+            test_path = os.path.join(base_dir, 'parsed_data', '_parsed_test.plk')
             self.load_preprocessed_data(training_path, test_path)
             chi_calculation(self, ratio)
 
